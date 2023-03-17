6851136643eb1cdd3c2c0072263795c011224ee3
Darina Gordeeva <dirtypresident@gmail.com>
2023-03-17 17:28:01 +0400

    bonusMile

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int bilet = 133;
+        int shag = 20;
+        int mileNaShag = 1;
+        int bonusNaBilet = bilet / shag * mileNaShag;
+        System.out.println("Начислено бонусов:" + bonusNaBilet);
+
+    }
+}
